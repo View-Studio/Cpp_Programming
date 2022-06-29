2acbd8e830c59df43b05f05b080eda8f573464dd
code-MIM <2153rud@gmail.com>
2022-06-29 22:59:20 +0900

    MY_Program(SelfMade_String)

--- MiniProject(Program)/SelfMade_StringClass/main.cpp
+++ MiniProject(Program)/SelfMade_StringClass/main.cpp
@@ -3,9 +3,29 @@
 
 int main()
 {
-	string st('K', 5);
-	string st1("HAHAHAHA");
-	st.printString();
+	string st1('K', 5);
+	string st2("Hello, World! My Name is KyungMin Kim");
+	string st3(st1);
+
+	std::cout << "[ PRINT STRING ]" << std::endl;
 	st1.printString();
-	//st1.printString();
+	st2.printString();
+	st3.printString();
+
+	std::cout << "\n[ STRLEN ]" << std::endl;
+    std::cout << st1.strlen() << std::endl;
+    std::cout << st2.strlen() << std::endl;
+    std::cout << st3.strlen() << std::endl;
+
+	std::cout << "\n[ ADD STRING ]" << std::endl;
+	st1.add_string(st2);
+	st1.printString();
+    std::cout << st1.strlen() << std::endl;
+
+	std::cout << "\n[ COPY STRING ]" << std::endl;
+	st1.copy_string(st2);
+	st1.printString();
+    std::cout << st1.strlen() << std::endl << std::endl;
+
+	return 0;
 }
--- MiniProject(Program)/SelfMade_StringClass/string.cpp
+++ MiniProject(Program)/SelfMade_StringClass/string.cpp
@@ -4,9 +4,16 @@ string::string(char c, int n)
 {
 	len = n;
 	str = new char[len + 1];
-	for (int i = 0; i < len + 1; ++i)
+	for (int i = 0; i < len+1; ++i)
 	{
-		str[i] = c;
+		if (i == len)
+		{
+			str[i] = '\0';
+		}
+		else
+		{
+			str[i] = c;
+		}
 	}
 }
 
@@ -28,33 +35,110 @@ string::string(const char* s)
 
 	str = new char[len + 1];
 
-	for (int i = 0; i < len; ++i)
+	for (int i = 0; i < len+1; ++i)
 	{
-		str[i] = s[i];
+		if (i == len)
+		{
+			str[i] = '\0';
+		}
+		else
+		{
+			str[i] = s[i];
+		}
 	}
 }
 
 string::string(const string& s)
 {
-	
+	len = s.len;
+	str = new char[len + 1];
+	for (int i = 0; i < len+1; ++i)
+	{
+		if (i == len)
+		{
+			str[i] = '\0';
+		}
+		else
+		{
+			str[i] = s.str[i];
+		}
+	}
 }
 
 string::~string()
 {
+	std::cout << "{Destructor Call}" << std::endl;
 	delete[] str;
 }
 
 void string::add_string(const string& s)
 {
+	int count = 0;
+	char* temp = new char[len + s.len + 1];
+
+	for (int i = 0; i < (len + s.len + 1); ++i)
+	{
+		if (i < len)
+		{
+			temp[i] = str[i];
+			++count;
+		}
+		else
+		{
+			if (i == len + s.len)
+			{
+				temp[i] = '\0';
+			}
+			else
+			{
+				temp[i] = s.str[i - count];
+			}
+		}
+	}
+
+	delete[] str;
+
+	str = temp;
+	len = this->strlen();
 }
 
 void string::copy_string(const string& s)
 {
+	delete[] str;
+	str = new char[s.len + 1];
+	len = s.len;
+
+	for (int i = 0; i < s.len + 1; ++i)
+	{
+		if (i == s.len)
+		{
+			str[i] = '\0';
+
+		}
+		else
+		{
+			str[i] = s.str[i];
+		}
+	}
 }
 
-int string::strlen() const
+int string::strlen()
 {
-	return len;
+	int count = 0;
+
+	while (true)
+	{
+		if (str[count] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			++count;
+		}
+	}
+
+	return count;
 }
 
 void string::printString()
