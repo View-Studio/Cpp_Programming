fc84534228cf24f9daf882582c5fb9df95774a0e
code-MIM <2153rud@gmail.com>
2022-08-02 06:43:20 +0900

    Rookiss_Cpp_5

--- Rookiss_Cpp/Maze/Player.cpp
+++ Rookiss_Cpp/Maze/Player.cpp
@@ -14,8 +14,72 @@ void Player::Init(Board* board)
 {
 	_pos = board->GetEnterPos();
 	_board = board;
+
+	Pos pos = _pos;
+
+	_path.clear();
+	_path.push_back(pos);
+
+	//목적지 도착하기 전에는 계속 실행
+	Pos dest = board->GetExitPos();
+
+	Pos front[DIR_COUNT] =
+	{
+		Pos{0, -1},		// UP
+		Pos{-1, 0},		// LEFT
+		Pos{0, 1},		// DOWN
+		Pos{1, 0}		// RIGHT
+	};
+
+	while (pos != dest)
+	{
+		// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인
+		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
+		if (CanGo(pos + front[newDir]))
+		{
+			// 오른쪽 방향으로 90도 회전
+			_dir = newDir;
+
+			// 앞으로 한 보 전진
+			pos += front[newDir];
+
+			_path.push_back(pos);
+		}
+		// 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인
+		else if (CanGo(pos + front[_dir]))
+		{
+			// 앞으로 한 보 전진
+			pos += front[_dir];
+
+			_path.push_back(pos);
+		}
+		else
+		{
+			// 왼쪽 방향으로 90도 회전
+			_dir =  (_dir + 1) % DIR_COUNT;
+		}
+	}
 }
 
 void Player::Update(uint64 deltaTick)
 {
+	if (_pathIndex >= _path.size())
+	{
+		return;
+	}
+
+	_sumTick += deltaTick;
+
+	if (_sumTick >= MOVE_TICK)
+	{
+		_sumTick = 0;
+		_pos = _path[_pathIndex];
+		++_pathIndex;
+	}
+}
+
+bool Player::CanGo(Pos pos)
+{
+	TileType tileType = _board->GetTileType(pos);
+	return tileType == TileType::EMPTY;
 }
--- Rookiss_Cpp/Maze/Player.h
+++ Rookiss_Cpp/Maze/Player.h
@@ -4,6 +4,11 @@ class Board;
 
 class Player
 {
+	enum
+	{
+		MOVE_TICK = 100,
+	};
+
 public:
 	Player();
 	~Player();
@@ -14,9 +19,15 @@ public:
 	void			SetPos(Pos pos) { _pos = pos; }
 	Pos				GetPos() { return _pos; }
 
+	bool			CanGo(Pos pos);
+
 private:
 	Pos				_pos = {};
 	int32			_dir = DIR_UP;
 	Board*			_board = nullptr;
+
+	vector<Pos>		_path;
+	uint32			_pathIndex = 0;
+	uint64			_sumTick = 0;
 };
 
